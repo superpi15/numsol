cfe1155d3fe2eb999156194ce5a72b747ab630d7
Nero Horus <superpi152@gmail.com>
2020-01-30 20:03:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,125 @@ typedef mp::number<mp::backends::cpp_int_backend<>, mp::et_off>       int_mp;
 typedef mp::number<mp::cpp_rational_backend, mp::et_off>    rational_mp;
 typedef mp::number<mp::cpp_dec_float<0>, mp::et_off>        float_mp;  // 0 means 'unlimited'
 
+
+
+
+/*--------------------------------------------------------------------------------------
+
+In-Tile load 
+
+--------------------------------------------------------------------------------------*/
+
+static double _sum_pow( int val, int nTerm, int coeff[], double denominator ){
+	double ret = 0;
+	for(int i=0; i<nTerm; i++)
+		ret += coeff[i] * pow( val, i+1 );
+	return ret/denominator;
+}
+
+
+double sum_pow1( int val ){
+	int nTerm = 2;
+	double denominator = 2;
+	int coeff[nTerm] = { 1, 1 };
+	return _sum_pow( val, nTerm, coeff, denominator );
+}
+
+double sum_pow2( int val ){
+	int nTerm = 3;
+	double denominator = 6;
+	int coeff[nTerm] = { 1, 3, 2 };
+	return _sum_pow( val, nTerm, coeff, denominator );
+}
+
+double sum_pow3( int val ){
+	int nTerm = 4;
+	double denominator = 4;
+	int coeff[nTerm] = { 0, 1, 2, 1 };
+	return _sum_pow( val, nTerm, coeff, denominator );
+}
+
+double sum_pow4( int val ){
+	int nTerm = 5;
+	double denominator = 30;
+	int coeff[nTerm] = { 1, 0, 10, 15, 6 };
+	return _sum_pow( val, nTerm, coeff, denominator );
+}
+
+double sum_pow5( int val ){
+	int nTerm = 6;
+	double denominator = 12;
+	int coeff[nTerm] = { 0, -1, 0, 5, 6, 2 };
+	return _sum_pow( val, nTerm, coeff, denominator );
+}
+
+double sum_pow( int val, int order ){
+	#define CND_RET(val,ORD) if( ORD==order ) return sum_pow ## ORD (val);
+	CND_RET(val,1);
+	CND_RET(val,2);
+	CND_RET(val,3);
+	CND_RET(val,4);
+	CND_RET(val,5);
+	assert(false);
+	return 0;
+}
+
+
+static double Gamma( int n ){
+	return (double) (pow(n,3) - n)/6.0f;
+}
+
+static double Lambda( int n, int k ){
+	return (double) Gamma(n-1) + (n-k) * sum_pow1(k);
+}
+
+static double Delta( int n, int k ){
+	return (double) Lambda(n,n-k) - Lambda(n,k-1);
+}
+
+
+static double Dpart( int n, bool isLoad ){
+	return 1/6.0f * sum_pow(n, 4+ isLoad ) 
+		- (n+1) * sum_pow(n, 3+ isLoad) 
+		+ (pow(n,2) +n   -1/6.0f) * sum_pow(n, 2+ isLoad ) 
+		+ (pow(n,2) +2*n +1) * sum_pow(n, 1+ isLoad );
+}
+
+static double F_Upart( int n, int m, bool isLoad ){
+	return 1/6.0f * 
+		( -sum_pow(m, 4+ isLoad)
+		+ 6*(n +1)* sum_pow(m, 3+ isLoad)
+		- (12*pow(n,2) + 24*n +11) * sum_pow(m, 2+ isLoad)
+		+ ( 8*pow(n,3) + 24 * pow(n,2) + 22 * n + 6) * sum_pow(m, 1+isLoad));
+}
+
+static double Upart( int n, bool isLoad ){
+	return F_Upart(n, 2*n, isLoad) - F_Upart(n, n, isLoad);
+}
+
+static double nInstance(int n){
+	return Dpart(n, false) + Upart(n, false);
+}
+
+static double nLoad(int n){
+	return Dpart(n, true ) + Upart(n, true );
+}
+
+double intile_expval(int n){
+	return nLoad(n)/nInstance(n);
+}
+double intile_ratio_expval2scale(int n){
+	return (nLoad(n)/nInstance(n))/n;
+}
+
+/*--------------------------------------------------------------------------------------
+
+END In-Tile load 
+
+--------------------------------------------------------------------------------------*/
+
+
+
 int_mp Factorial(int number) 
 { 
 	int_mp num = 1; 
@@ -75,17 +194,59 @@ double probability( int K, int Th, int N ){
 	return 100 * ret;
 }
 
-int main(){
-	int K = 128; // num bucket
+
+using namespace std;
+void plot_curve( int ScaleFactor, int Th ){
+	double inTile = intile_ratio_expval2scale(ScaleFactor);
+	cout << "In-Tile Factor = "<< inTile <<endl;
+	char buff[1024];
+	int NumTile = pow( ScaleFactor, 2 );
+	sprintf( buff, "%5s %12s %12s %15s %15s %15s", "/", "NumTile", "Step", "Safe-Prob.", "AvgPrefill", "+In-Tile" );
+	cout << buff << endl;
+	for(int i=0; i<100; i++){
+		int Load = NumTile * Th * 0.01 * i;
+		double AvgPrefill = (double) Load / NumTile / Th;
+		double InTileAdj  = AvgPrefill / inTile;
+		double prob = probability( NumTile, Th, Load );
+		sprintf( buff, "%5s %12d %12d %15.7f %15.7f %15.7f", "val", NumTile, i, prob, AvgPrefill, InTileAdj );
+		cout << buff <<endl;
+		if( prob < 50 )
+			break;
+	}
+}
+
+int main( int argc, char * argv[] ){
+	if( argc < 3 ){
+		cout<<"./prog [ScaleFactor] [Threshold]"<< endl;
+		return 0;
+	}
+	plot_curve( atoi(argv[1]), atoi(argv[2]) );
+	return 0;
+
+
+	int K = 3; // num bucket
 	int Th= 60;  // threshold
 	
-
-	for(int i=2; i<10; i++){
-		int Ni= K * Th * i * 0.1 ;
-		//if( 472 != Ni )\
-			continue;
-		double prob = probability(K, Th, Ni);\
-		std::cout << "Given "<< std::setw(3) << Ni <<" candys "<< std::setw(3) << K << " buckets, the probability of all legal is ";\
-		std::cout << std::setw(17) << (prob<0.01? 0: prob) << " \%" << ". Avg Prefill = "<< (double)(Ni/K)/Th <<std::endl;\
-	}
+	double SafePrefill = 0;
+//	for(int i=1; i<100; i++){
+//		int Ni= K * Th * i * 0.01 ;
+//		//if( 472 != Ni )\
+//			continue;
+//		double prob = probability(K, Th, Ni);\
+//		if( prob < 0.01 )
+//			break;
+//		std::cout << "Given "<< std::setw(3) << Ni <<" candys "<< std::setw(3) << K << " buckets, the probability of all legal is ";\
+//		std::cout << std::setw(17) << (prob<0.01? 0: prob) << " \%" << ". Avg Prefill = "<< (double)(Ni/K)/Th <<std::endl;\
+//	}
+//	for(int i=1; i<100; i++){
+//		int Ni= K * Th * i * 0.01 ;
+//		//if( 472 != Ni )\
+//			continue;
+//		double prob = probability(K, Th, Ni);\
+//		if( prob < 0.01 )
+//			break;
+//		;
+//		//std::cout << "Given "<< std::setw(3) << Ni <<" candys "<< std::setw(3) << K << " buckets, the probability of all legal is ";\
+//		std::cout << std::setw(17) << (prob<0.01? 0: prob) << " \%" << ". Avg Prefill = "<< (double)(Ni/K)/Th <<std::endl;\
+//	}
 }
